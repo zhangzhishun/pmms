034466183bedeac0bfa81f05a4e241a5d3be4cee
张智顺 <5611608+hbuzzs@user.noreply.gitee.com>
2020-01-23 21:03:47 +0800

    配合前端修改bug

---
+++ src/main/java/com.springboot/until/FileUtils.java
@@ -0,0 +1,38 @@
+package com.springboot.until;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author eternalSy
+ * @version 1.0.0
+ */
+public class FileUtils {
+
+
+    /**
+     * @param file 文件
+     * @param filePath 文件存放路径
+     * @param fileName 源文件名
+     * @return
+     */
+    public static void upload(byte[] file, String filePath, String fileName) throws Exception {
+        File targetFile = new File(filePath);
+        if (!targetFile.exists()) {
+            targetFile.mkdirs();
+        }
+        FileOutputStream out = new FileOutputStream(filePath + fileName);
+        out.write(file);
+        out.flush();
+        out.close();
+    }
+
+    public static synchronized String createtFileName(String suffix) {
+        java.util.Date dt = new java.util.Date(System.currentTimeMillis());
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        String fileName= fmt.format(dt);
+        fileName = fileName + suffix;
+        return fileName;
+    }
+}
